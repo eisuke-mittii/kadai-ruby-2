3fd7776d32ba7690bb7013e7d52e7de47dd3c225
eisuke.mitsu <m.eisuke1229@gmail.com>
2021-12-03 09:31:22 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(num)
+    if (num) % 3 == 0 && (num) % 5 == 0
+        puts "fizzbuzz"
+    elsif (num) % 3 == 0
+        puts "fizz"
+    elsif (num) % 5 == 0
+        puts "buzz"
+    else
+        puts (num)
+    end
+end
+num_max = 100
+(1..num_max).each do |number|
+    fizzbuzz number
+end
+
+    
